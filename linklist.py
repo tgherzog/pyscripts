2fe4dfe6f42b894ee6172c49b36dd88a9db030fd
Tim Herzog <timgherzog@gmail.com>
2016-07-24 22:53:48 -0400

    Fixes to linklist.py

--- linklist.py
+++ linklist.py
@@ -4,20 +4,29 @@
 Crawls a site and reports a list of all links
 
 Usage:
-  linklist.py [--depth=NUM] URL
+  linklist.py [--depth=NUM --print-links --print-images] URL
 
 Options:
-  --depth:          maximum recursive depth, 0 disables recursive scanning  (default: 0)
+  --depth:          maximum recursive depth, 0 disables recursive scanning  [default: 0]
+  --print-links:    print hyperlinks only
+  --print-images:   print image links only
 
 """
 
 from docopt import docopt
 import requests
 import re
+import sys
 import urlparse
 from BeautifulSoup import BeautifulSoup
 
 config = docopt(__doc__, version="version " + "0.1")
+
+if config['--print-images'] == False and config['--print-links'] == False:
+  config['--print-images'] = True
+  config['--print-links']  = True
+
+config['--verbose'] = True
 
 base_url = config['URL']
 base_url_parts = urlparse.urlparse(base_url, 'http')
@@ -37,15 +46,31 @@ def task_key(url):
     parts = parts._replace(scheme='')
     return urlparse.urlunparse(parts).lower()
     
-def harvest(url):
+def harvest(url, origUrl, srcUrl):
     global links, images, task_list, base_url_parts, config, depth
 
+    # print >> sys.stderr, "Tasks: ", task_list
+
+    url_ = url
+    url = re.sub('#.+', '', url)
+    if task_list.get(task_key(url)) == True:
+      if config['--verbose']:
+      	print >> sys.stderr, "Skipping %s" % (url_)
+
+      return
 
     task_list[task_key(url)] = True
 
-    print "Scanning %s (level %d)" % (url, depth)
+    if config['--verbose']:
+    	print >> sys.stderr, "Scanning %s (level %d)" % (url, depth)
 
     response = requests.get(url)
+    if response.status_code != 200:
+      if config['--verbose']:
+	print >> sys.stderr, "  Error: %d, Original: %s, From: %s" % (response.status_code, origUrl, srcUrl)
+
+      return
+
     soup = BeautifulSoup(response.text)
 
     # harvest images
@@ -53,9 +78,9 @@ def harvest(url):
       src = img.get('src')
       if src is not None:
         if images.get(src) is None:
-            images[src] = 1
+            images[src] = [url]
         else:
-            images[src] = images[src] + 1
+            images[src].append(url)
 
     # harvest links
     for link in soup.findAll('a'):
@@ -68,28 +93,34 @@ def harvest(url):
         if parts.netloc == '':
             parts = parts._replace(netloc=base_url_parts.netloc)
 
-        if len(parts.path) > 0 and parts.path[0] != '/':
-            parts = parts._replace(path=base_url_parts.scheme + '/' + parts.path)
+        if parts.path == "" or parts.path[0] != '/':
+            parts = parts._replace(path=base_url_parts.path + '/' + parts.path)
 
         parts = parts._replace(scheme=parts.scheme.lower())
         parts = parts._replace(netloc=parts.netloc.lower())
 
         full_link = urlparse.urlunparse(parts)
         if links.get(full_link) is None:
-          links[full_link] = 1
+          links[full_link] = [url]
         else:
-          links[full_link] = links[full_link] + 1
+          links[full_link].append(url)
 
-        if config.get('--depth') and depth < config['--depth']:
+        if depth < int(config['--depth']):
             if parts.scheme == 'http' or parts.scheme == 'https':
-                if parts.netloc == base_url_parts.netloc and task_list.get(task_key(full_link)) is None:
+                if parts.netloc == base_url_parts.netloc:
                     depth = depth + 1
-                    harvest(full_link)
+                    harvest(full_link, href, url)
                     depth = depth - 1
 
 
-harvest(base_url)
+harvest(base_url, base_url, '(TOP)')
 
-print "Links:"
-for url,count in links.iteritems():
-  print "  %4d %s" % (count, url)
+if config['--print-links']:
+  for url,sources in links.iteritems():
+    # print "  %4d %s" % (count, url)
+    print "%s %s" % (url, ", ".join(sources))
+
+if config['--print-images']:
+  for url,sources in images.iteritems():
+    # print "  %4d %s" % (count, url)
+    print "%s %s" % (url, ", ".join(sources))
