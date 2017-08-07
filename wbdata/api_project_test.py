29a9cd8801bacc75c83570f40e75528c09863271
Tim Herzog <therzog1@worldbank.org>
2017-08-07 18:52:50 -0400

    Add code mapping to api_project_test.py

--- wbdata/api_project_test.py
+++ wbdata/api_project_test.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python
 
+
+# country code mapping across platforms
+code_mapping = {
+  'CD': 'ZR',
+  'KV': 'XK',
+  'TL': 'TP',
+  'YE': 'RY',
+}
+
 base = 'http://search.worldbank.org/api/v2/projects?format=json&rows=20000&prodline_exact=GU^PE&status_exact=Active^Closed'
 url1 = base + '&fl=countrycode,boardapprovaldate,totalamt&countrycode_exact=%s&frmYear=1947&toYear=2015' % ('PE')
 
